aa7cbfb9925e3663a3f4adec3907263f98b1caff
Simone Tesini <simozilla006@gmail.com>
2025-07-31 17:02:33 +0200

    first commit

---
+++ backend/src/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/api")
+def index():
+    return "hello from flask"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { onMount } from "svelte"
+
+    let text = $state("...")
+    onMount(async () => {
+        let res = await fetch("/api")
+        text = await res.text()
+    })
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>{text}</p>
